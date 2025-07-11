b53ecbae2bf56d35aa2ce82f67f84870ea6d3b7e
Marcus Veres <marcus.veres@gmail.com>
2025-07-11 16:36:25 -0400

    FIX :: fixes critical error that was crashing php

--- pcr-hello-biz/inc/theme-setup.php
+++ pcr-hello-biz/inc/theme-setup.php
@@ -17,15 +17,15 @@ function pcr_enqueue_child_styles() {
         'hello-biz-parent',
         get_template_directory_uri() . '/style.css',
         array(),
-        wp_get_theme()->get_template_version()
+        wp_get_theme(get_template())->get('Version') // Fixed: get parent theme version
     );
     
-    // Child theme CSS (note: styles.css not style.css)
+    // Child theme CSS
     wp_enqueue_style(
         'pcr-hello-biz-child',
-        get_stylesheet_directory_uri() . '/styles.css',
+        get_stylesheet_directory_uri() . '/style.css', // Fixed: was styles.css
         array('hello-biz-parent'),
-        wp_get_theme()->get('Version')
+        wp_get_theme()->get('Version') // Child theme version
     );
 }
 add_action('wp_enqueue_scripts', 'pcr_enqueue_child_styles');
